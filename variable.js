de67cf3a91b9fff5c7a2634fc6e5e7b363c271e0
arafakst <arafakst@gmail.com>
2023-02-24 00:59:55 +0600

    js variable

--- variable.js
+++ variable.js
@@ -1,6 +1,18 @@
 // vary+able=variable
+
+// numeric
 var price = 13;
 var age = 17;
 var temperature = 38;
 var weight = 60;
 var money = 1000;
+
+//string
+var person = "sadar uddin";
+var location = "andor bandorban";
+var special = 'alia vaat';
+
+// Boolean
+var serious = true;
+var ifFullMarks = true;
+var isSingle = true;
