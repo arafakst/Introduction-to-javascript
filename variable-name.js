419f140df3a53f5ffb50289324ba3761bef29cc1
arafakst <arafakst@gmail.com>
2023-02-24 01:34:33 +0600

    variable naem

---
+++ variable-name.js
@@ -0,0 +1,28 @@
+var price = 29;
+
+
+// 1.variable name can not be any keyword
+var false = 96;
+var return =true;
+
+// 2. variable name has to be in one word. No space
+var my home address = "hulululu"
+
+// 3.variable name can not have quotation
+var 'name'= "tom hang";
+
+// 4.variable name can not start with a number but cand ends with a number
+var 99club = 1998;
+var club23 = 2222;
+
+// 5.How to use long names
+
+var user-name = "rajbaappa";
+
+var user_name = "bappa raj";
+var userHomeAddress = "andor killa bandorban"  //camel case
+var UserHomeAddress = "andor killa bandorban" //pascal case
+ //Variable name case sensetive
+
+ var person = "Halim";
+ var Person = "Halim";
