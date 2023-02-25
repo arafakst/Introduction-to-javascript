73b6518cd625697d861a2066a68070c9b92e5815
arafakst <arafakst@gmail.com>
2023-02-25 23:50:52 +0600

    js mathematical operation shorthand

---
+++ shorthand.js
@@ -0,0 +1,53 @@
+//Variable Declaration
+var tomatoPrice = 25;
+
+//Variable value Updated 
+tomatoPrice = 30;
+
+//add something to the previous value
+
+var price1 = 31;
+var price2 = 20;
+
+//set a new value
+//price1 = 35;
+
+//add some value to the existing value
+price1 = price1 + 10;
+//console.log(price1);  
+
+// addition to the same variable shorthand
+price1 += 10;
+
+price2 = price2 - 5;
+
+// shorthand
+price2 -= 5;
+console.log(price1); 
+
+//*=
+price1 *= 5;
+console.log(price1); 
+
+// /=
+price1 /= 5;
+console.log(price1); 
+
+var age = 15;
+age = age + 1;
+//means adding 1 to the existing value
+age++;
+
+var love = 100;
+//love = love-1;
+
+//deduction 1 from exsisting value
+love--;
+console.log(love); 
+
+
+
+
+
+
+
