3c5171468b5fa481bf264208cbfd9e29de74b221
arafakst <arafakst@gmail.com>
2023-02-26 00:50:25 +0600

    remainder in js

---
+++ remainder.js
@@ -0,0 +1,10 @@
+var mangoes = 19;
+var person = 7;
+var remainder = mangoes % person;
+
+// / This means vaggol
+//% thid means vagsesh
+//console.log(mangoes/person);
+// console.log(mangoes%person);
+console.log(remainder);
+
